6a60ef3ec7213945da33b604176466636af3807c
Andy Kreling <krelinga@gmail.com>
2024-07-07 21:48:18 +0000

    introduce a helper function to translate enum names into human-readable values.

--- nextjs-tcserver/app/ui/table.tsx
+++ nextjs-tcserver/app/ui/table.tsx
@@ -1,4 +1,20 @@
-import { ListAsyncTranscodesResponse } from '@buf/krelinga_proto.bufbuild_es/krelinga/video/tcserver/v1/tcserver_pb'
+import {
+  ListAsyncTranscodesResponse,
+  TranscodeState,
+  ListAsyncTranscodesResponse_Op_Type,
+} from '@buf/krelinga_proto.bufbuild_es/krelinga/video/tcserver/v1/tcserver_pb'
+import { EnumType, proto3 } from '@bufbuild/protobuf'
+
+// TODO: it would be better to use a concrete type for [type], but it's too hard
+// to figure out how to import the right symbol for now.
+function enumName(type: any, value: number): string {
+  const t = proto3.getEnumType(type)
+  var name = t?.findNumber(value)?.localName
+  if (!name) {
+    name = '???'
+  }
+  return name
+}
 
 export default function Table({ reply } : { reply?: ListAsyncTranscodesResponse; }) {
   return (
@@ -14,8 +30,8 @@ export default function Table({ reply } : { reply?: ListAsyncTranscodesResponse;
         { reply?.op?.map(op => (
           <tr key={op.name}>
             <td>{op.name}</td>
-            <td>{op.type}</td>
-            <td>{op.state}</td>
+            <td>{enumName(ListAsyncTranscodesResponse_Op_Type, op.type)}</td>
+            <td>{enumName(TranscodeState, op.state)}</td>
           </tr>
         ))}
       </tbody>
