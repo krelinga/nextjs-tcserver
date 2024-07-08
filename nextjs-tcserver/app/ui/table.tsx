4d73fb009eaf02e082653412b74dd06558925280
Andy Kreling <krelinga@gmail.com>
2024-07-08 03:13:21 +0000

    make links to individual op status in root page.

--- nextjs-tcserver/app/ui/table.tsx
+++ nextjs-tcserver/app/ui/table.tsx
@@ -1,9 +1,26 @@
 import {
   ListAsyncTranscodesResponse,
   TranscodeState,
+  ListAsyncTranscodesResponse_Op,
   ListAsyncTranscodesResponse_Op_Type,
+
 } from '@buf/krelinga_proto.bufbuild_es/krelinga/video/tcserver/v1/tcserver_pb';
 import { enumName } from '@/app/lib/enums'
+import Link from 'next/link';
+
+function makeHref(op: ListAsyncTranscodesResponse_Op): string {
+  var prefix = '';
+  if (op.type === ListAsyncTranscodesResponse_Op_Type.SINGLE_FILE) {
+    prefix = 'single_file';
+  } else if (op.type === ListAsyncTranscodesResponse_Op_Type.SHOW) {
+    prefix = 'show';
+  } else if (op.type === ListAsyncTranscodesResponse_Op_Type.SPREAD) {
+    prefix = 'spread'
+  } else {
+    throw new Error(`unhandled op type ${op.type}`);
+  }
+  return `/${prefix}/${op.name}`
+}
 
 export default function Table({ reply } : { reply?: ListAsyncTranscodesResponse; }) {
   return (
@@ -18,7 +35,11 @@ export default function Table({ reply } : { reply?: ListAsyncTranscodesResponse;
       <tbody>
         { reply?.op?.map(op => (
           <tr key={op.name}>
-            <td>{op.name}</td>
+            <td>
+              <Link href={makeHref(op)}>
+                {op.name}
+              </Link>
+            </td>
             <td>{enumName(ListAsyncTranscodesResponse_Op_Type, op.type)}</td>
             <td>{enumName(TranscodeState, op.state)}</td>
           </tr>
