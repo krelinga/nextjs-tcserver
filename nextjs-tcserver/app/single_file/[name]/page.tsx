874ed0a8743f96e826c2f28f1b95ad1a692e9f34
Andy Kreling <krelinga@gmail.com>
2024-07-08 02:48:38 +0000

    get the single_file page to read status.

--- nextjs-tcserver/app/single_file/[name]/page.tsx
+++ nextjs-tcserver/app/single_file/[name]/page.tsx
@@ -1,7 +1,21 @@
-export default async function SingleFile() {
+import { client } from '@/app/lib/grpc-service';
+import { CheckAsyncTranscodeRequest, CheckAsyncTranscodeResponse } from '@buf/krelinga_proto.bufbuild_es/krelinga/video/tcserver/v1/tcserver_pb'
+
+export default async function SingleFile({
+  params
+}: {
+  params: {
+    name: string
+  }
+}) {
+  const req = new CheckAsyncTranscodeRequest({
+    name: params.name,
+  })
+  const resp = await client.checkAsyncTranscode(req)
   return (
-    <p>
-      Got here!
-    </p>
+    <div>
+      <h1>{params.name}</h1>
+      <p>State: {resp.state}</p>
+    </div>
   );
 }
